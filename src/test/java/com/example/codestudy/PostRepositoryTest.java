d4437df68540f2afb78466df4f04fb702cafb26b
minuk926 <minuk926@gmail.com>
2021-07-21 05:35:22 +0900

    테스트 진행

--- src/test/java/com/example/codestudy/PostRepositoryTest.java
+++ src/test/java/com/example/codestudy/PostRepositoryTest.java
@@ -5,21 +5,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @TestPropertySource("classpath:application-test.properties")
 public class PostRepositoryTest {
